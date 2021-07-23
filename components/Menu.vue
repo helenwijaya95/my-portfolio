425e70a64909973d27e092033991411a5f620e79
helenwijaya <helen.wijaya95@gmail.com>
2021-07-23 22:52:27 +0700

    Add menu

---
+++ components/Menu.vue
@@ -0,0 +1,75 @@
+<template>
+  <header>
+    <a class="menu-btn" @click.prevent="toggleMenu">Menu</a>
+    <transition name="fade" mode="out-in">
+      <div v-if="show" class="menu-wrapper">
+        <ul class="menu">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Me</a></li>
+          <li><a href="#">My Journey</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
+      </div>
+    </transition>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data() {
+    return {
+      show: false,
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.show = !this.show
+    },
+  },
+}
+</script>
+
+<style scoped>
+.menu-btn {
+  position: fixed;
+  right: 0;
+  top: 0;
+  padding: 20px;
+  z-index: 2;
+  color: black;
+}
+
+.menu-wrapper {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  z-index: 1;
+  right: 0;
+  top: 0;
+  background-color: rgb(208 57 57 / 98%);
+}
+
+.menu {
+  list-style: none;
+  font-size: 5.2rem;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  line-height: 8rem;
+}
+
+.menu li a {
+  color: black;
+  text-decoration: none;
+}
+
+/* fade animation */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
